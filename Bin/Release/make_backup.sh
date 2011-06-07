5931663de143cdb7c66b4479491ea3e2caa92886
Danilo Ávila <daniloavilaf@gmail.com>
2011-06-07 10:51:30 -0300

    desenvolvendo script para remover usuarios

--- Bin/Release/make_backup.sh
+++
@@ -1,5 +0,0 @@
-if [$1 =  '']; then
-	echo 'passe o numero da nova versão como parâmetro'
-else
-	tar -cvf backup/data_$1.tar out_averageImage.bmp out_eigenfaces.bmp data/* facedata.xml train.txt
-fi
